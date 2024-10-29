5a6c786756dab8c34f731b7630f0de137cbf833f
Ryan Merlini <rdmerlini@gmail.com>
2024-10-29 15:08:19 -0400

    added a user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,29 +1,41 @@
-import { Schema, Document, model, ObjectId } from 'mongoose';
+import mongoose, { Schema, Document, Model } from 'mongoose';
 
 interface IUser extends Document {
-  first: string;
-  last: string;
-  age: number;
-  videos: ObjectId[];
-  fullName: string;
- }
+  username: string;
+  email: string;
+  thoughts: mongoose.Types.ObjectId[];
+  friends: mongoose.Types.ObjectId[];
+  friendCount: number;
+}
 
-// Schema to create User model
 const userSchema = new Schema<IUser>(
   {
-    first: String,
-    last: String,
-    age: Number,
-    videos: [
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Please enter a valid email address'],
+    },
+    thoughts: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'video',
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
       },
     ],
   },
   {
-    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
     toJSON: {
       virtuals: true,
     },
@@ -31,21 +43,10 @@ const userSchema = new Schema<IUser>(
   }
 );
 
-// Create a virtual property `fullName` that gets and sets the user's full name
-userSchema
-  .virtual('fullName')
-  // Getter
-  .get(function (this: any) {
-    return `${this.first} ${this.last}`;
-  })
-  // Setter to set the first and last name
-  .set(function (this: any, v: any) {
-    const first = v.split(' ')[0];
-    const last = v.split(' ')[1];
-    this.set({ first, last });
-  });
+userSchema.virtual('friendCount').get(function (this: IUser) {
+  return this.friends.length;
+});
 
-// Initialize our User model
-const User = model('user', userSchema);
+const User: Model<IUser> = mongoose.model<IUser>('User', userSchema);
 
-export default User
+export default User;
